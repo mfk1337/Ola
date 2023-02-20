bcc2ddd82a8baa528a0d2a561c36a39b8d93e1d3
FAK <fak@pentia.dk>
2023-02-20 13:18:43 +0100

    first commit

---
+++ App.tsx
@@ -0,0 +1,23 @@
+/**
+ * Ola - Chat App
+ * https://github.com/facebook/react-native
+ */
+
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+const App = () =>{
+
+  return(
+    <SafeAreaView style={{justifyContent: 'center', flex: 1, alignItems: 'center'}}>
+      <Text style={{fontSize:40}}>
+        Ola
+      </Text>
+    </SafeAreaView>
+  )
+
+}
+
+
+
+export default App;
