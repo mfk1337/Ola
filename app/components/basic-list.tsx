d664533ad4af40a0d3c13978151af0f3c856f189
FAK <fak@pentia.dk>
2023-02-23 14:59:17 +0100

    pull to refresh added to Chat room list

--- app/components/basic-list.tsx
+++ app/components/basic-list.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
-import { View, ViewStyle, StyleProp, FlatList, ListRenderItem} from 'react-native';
+import { View, ViewStyle, StyleProp, FlatList, ListRenderItem, RefreshControl} from 'react-native';
 
 interface BasicListProps {
     style?: StyleProp<ViewStyle>;
     data: any; 
     renderItem: ListRenderItem<any>;
+    onRefresh: () => void;
+    refreshing: boolean;
 }
 
 export const BasicList: React.FC<BasicListProps> = props => {
 
-    const { style, data, renderItem } = props;
+    const { style, data, renderItem, onRefresh,refreshing } = props;
 
     return <FlatList style={[style]}
     data={data}
     ItemSeparatorComponent={() => <View style={{height: 3}} />}
-    renderItem={renderItem}/>  
+    renderItem={renderItem}
+    onRefresh={onRefresh}
+    refreshing={refreshing}
+    />  
 
 }
--- app/screens/rooms.screen.tsx
+++ app/screens/rooms.screen.tsx
@@ -12,8 +12,9 @@ import { Colors } from "../assets/styles/colors";
 
 export const RoomsScreen = () => {
 
-    const [loading, setLoading] = useState(true); // Set loading to true on component mount
-    const [chatrooms, setChatrooms] = useState([]); // Initial empty array of users
+    const [loading, setLoading] = useState(true); 
+    const [refreshingList, setRefreshingList] = useState(false); 
+    const [chatrooms, setChatrooms] = useState([]); 
 
     useEffect(() => {
         const subscriber = firestore()
@@ -31,10 +32,7 @@ export const RoomsScreen = () => {
             console.log("lol")
             setChatrooms(chatrooms);
             setLoading(false);
-          });
-      
-        // Unsubscribe from events when no longer in use
-        //return () => subscriber();
+          });      
       }, []);
 
 
@@ -42,7 +40,31 @@ export const RoomsScreen = () => {
         <SafeAreaView style={[sharedStyles.container]}>
             <Header title='Chat rooms' style={{textAlign: 'center'}}/>       
 
-            <BasicList style={styles.basicListStyle} data={chatrooms} renderItem={({item}) => <BasicListItem item={item} />}/>                              
+            <BasicList style={styles.basicListStyle} data={chatrooms} renderItem={({item}) => <BasicListItem item={item}/>}
+            onRefresh={() => {
+              console.log("Refreshing list...")
+              // TODO: Tidy up this code
+              setRefreshingList(true)
+              const subscriber = firestore()
+                .collection('chatrooms')
+                .get()
+                .then(querySnapshot => {
+                  const chatrooms = [];
+            
+                  querySnapshot.forEach(documentSnapshot => {
+                      chatrooms.push({
+                      ...documentSnapshot.data(),
+                      key: documentSnapshot.id,
+                    });
+                  });
+                  console.log("List refreshed...")
+                  setRefreshingList(false)
+                  setChatrooms(chatrooms);
+                });
+            
+            }}
+            refreshing={refreshingList}
+            />                              
            
             <Button title="Log off" onPress={()=>{
                 auth()
